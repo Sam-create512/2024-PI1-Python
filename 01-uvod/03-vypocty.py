a3eb890e33091f4fc5a7a96ab2fa75d405409fca
Samuel Šutek <suteksamuel02@gmail.com>
2024-09-25 09:15:28 +0200

    Add vstup-vystupand vypocty to 01-uvod

---
+++ 01-uvod/03-vypocty.py
@@ -0,0 +1,10 @@
+a = int(input("Zadaj číslo a: ")) # Input je príkazom vstupu. Vypíše nám na obrazovku text v zátvorke a to, čo užívateľ napíše na klávesnici, uloží do premennej a
+b = int(input("Zadaj číslo b: ")) # Input vracia vždy výsledok vo formáte reťazca (postupnosť znakov resp. text), funkcia int() pretypuje reťazecna celé číslo
+sucet = a + b
+print("Súčet je: ", sucet)
+rozdiel = a - b
+print("Rozdiel je: ", rozdiel)
+sucin = a * b
+print("Súčin je: ", sucin)
+podiel = a / b
+print("Podiel je ", podiel)
---
+++ 01-uvod/04-menoavek.py
@@ -0,0 +1,6 @@
+meno = input("Zadaj meno: ")
+priezvisko = input("Zadaj priezvisko: ")
+rok = int(input("Zadaj rok narodenia: "))
+vek = 2024 - rok
+print("Meno a priezvisko: ", meno , priezvisko)
+print("Vek: ", vek)
