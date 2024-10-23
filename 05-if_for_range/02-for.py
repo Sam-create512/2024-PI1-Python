eac733f0ff36f068052d6a09c5dc48bef61b00a1
Samuel Šutek <suteksamuel02@gmail.com>
2024-10-23 10:03:30 +0200

    Add-05

---
+++ 05-if_for_range/02-for.py
@@ -0,0 +1,20 @@
+# for i in 0, 1, 3, 4:
+#     print( i ,"Ahoj")
+
+# for i in 10, 1, 344, 4414:
+#         print( i ,"Ahoj")
+
+# for deň in "Po", "Ut", "St":
+#     print(deň)
+
+# for i in range(5):  # i pojde od 0..4
+#     print(i)
+#     print("Ahoj")
+
+# for i in range(3, 5):  # i pojde od 3 do 4
+#     print(i)
+#     print("Ahoj")
+
+for i in range(1, 10, 2):  # i pojde 1,3,5,7,9
+    print(i)
+    print("Ahoj")
